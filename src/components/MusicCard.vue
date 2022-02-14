7d4ce30d57c0f3168d6f94942231e270c6562b96
Terra Smith <t_m@linuxmail.org>
2022-02-14 07:02:05 -0800

    code clean-up

--- src/components/MusicCard.vue
+++ src/components/MusicCard.vue
@@ -2,24 +2,19 @@
 	<div>
 		<div class="card music-card">
 			<div class="image-wrapper">
-				<!-- Image Goes Here -->
 				<img :src="music.image[4]['#text']" />
 			</div>
 			<div class="info-wrapper flex-col">
-				<!-- Music Name Goes Here -->
 				<h3>{{ music.name }}</h3>
 			</div>
 		</div>
 	</div>
 </template>
-<!-- @click="selectMusic(music.name)" -->
 <script>
 export default {
 	name: 'MusicCard',
 
 	props: ['music'],
-	// chartMusic: [],
-	// playlists: {}
 	methods: {
 		selectMusic(musicName) {
 			this.$emit('selectMusic', musicName);
