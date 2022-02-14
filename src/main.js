383a007fc9530277448f1ffb17e30bcd6813c369
Terra Smith <t_m@linuxmail.org>
2022-02-14 02:57:08 -0800

    modified main.js -- changed baseURL

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
+
 // axios.defaults.baseURL = 'http://127.0.0.1:8000'
 axios.defaults.baseURL = 'https://anthem-music.herokuapp.com'
 
